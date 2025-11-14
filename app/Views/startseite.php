63d3cd7c1a14b2120c140bb4b00d7c33ddf8cdaa
Jus10eR <84348179+Jus10eR@users.noreply.github.com>
2025-11-14 01:19:16 +0100

    Aufgabe 1 - statische Webseite

---
+++ app/Views/startseite.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+<!-- NAVBAR -->
+<link rel="stylesheet" href="style.css">
+
+<nav class="navbar navbar-expand-lg navbar-custom">
+    <div class="container-fluid">
+
+        <!-- Logo + Text -->
+        <a class="navbar-brand d-flex align-items-left ms-3" href="#">
+            <img src="logo.svg" alt="Logo" class="navbar-logo">
+        </a>
+
+        <!-- Navigation -->
+        <div class="navbar-collapse show" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="#">Tasks</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Boards</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Spalten</a></li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
+
+<!-- INHALT -->
+<div class="container mt-4">
+    <div class="card">
+        <div class="card-header">
+            <h2 class="card-title">Tasks</h2>
+        </div>
+        <div class="card-body">
+            <p></p>
+        </div>
+    </div>
+</div>
+
+<footer class="footer-custom">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+
+        <!-- Linker Text -->
+        <span>©Web-Entwicklung 2023</span>
+
+        <!-- Rechte Links -->
+        <div>
+            <a href="#">Impressum</a>
+            <a href="#">Datenschutz</a>
+            <a href="#">Kontakt</a>
+        </div>
+
+    </div>
+</footer>
+
+
+<!-- Bootstrap JS -->
+<script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+</script>
+
+</body>
+</html>
