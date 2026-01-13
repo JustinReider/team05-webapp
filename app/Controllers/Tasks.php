1c316981286c243457806402c48451851093e378
salo <salomeparedes2000@gmail.com>
2026-01-13 19:56:52 +0100

    Update Tasks.php

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -14,9 +14,28 @@ class Tasks extends BaseController
 	}
 	public function getNew()
 	{
-    $model = new TasksModel();
-	$data['title'] = 'Neue Task erstellen';
-	$data['task'] = null;
-	return view(pages/tasks_form', $data);
+    	$model = new TasksModel();
+		$data['title'] = 'Neue Task erstellen';
+		$data['task'] = null;
+		return view(pages/tasks_form', $data);
 	}
+
+	public function postSave()
+    {
+    	$model = new TasksModel();
+        $saveData = [
+            'tasks'            => $this->request->getPost('tasks'),
+            'taskartenid'      => $this->request->getPost('taskartenid'),
+            'personenid'       => $this->request->getPost('personenid'),
+            'spaltenid'        => $this->request->getPost('spaltenid'),
+            'erinnerungsdatum' => $this->request->getPost('erinnerungsdatum'),
+            'notiz'            => $this->request->getPost('notiz'),
+            'erinnerung'       => $this->request->getPost('erinnerung'),
+            'erledigt'         => 0,
+            'geloescht'        => 0  
+        ];
+		
+		$model->insert($saveData);
+        return redirect()->to(base_url('tasks'));
+    }
 }
