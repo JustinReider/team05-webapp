c4dc1c720437d0b103df49140fac193f91bcc3c0
salo <salomeparedes2000@gmail.com>
2026-01-13 20:04:13 +0100

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,3 +9,4 @@ $routes->setAutoRoute(true);
 $routes->get('/', 'Home::index');
 $routes->get('/spalten', 'Spalten::index');
 $routes->get('/spalten/new', 'Spalten::new');
+$routes->get('tasks/new', 'Tasks::getNew');
